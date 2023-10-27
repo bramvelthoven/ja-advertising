25cce836cb460bf996f9f0d785e8c3b987952c75
Bram Velthoven <bram.velthoven@finalist.nl>
2023-10-27 17:35:02 +0200

    JADEV-8 revert webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,5 @@
 let mix = require('laravel-mix');
 const {sourceMaps} = require("laravel-mix");
-require('laravel-mix-imagemin');
 
 require('laravel-mix-copy-watched');
 
@@ -10,22 +9,5 @@ mix.js('resources/js/app.js', 'public/js')
         processCssUrls: false,
     })
     .sourceMaps()
-    .imagemin(
-        'img/**.*',
-        {
-            context: 'resources',
-        },
-        {
-            optipng: {
-                optimizationLevel: 5
-            },
-            jpegtran: null,
-            plugins: [
-                require('imagemin-mozjpeg')({
-                    quality: 100,
-                    progressive: true,
-                }),
-            ],
-        }
-    );
+    .copyDirectory('resources/images', 'public/images');
 
